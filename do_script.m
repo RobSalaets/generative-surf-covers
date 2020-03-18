c069de38375618635c448e7474b7df1d058e5201
Rob Salaets <r0625918@moeskroen.cs.kotnet.kuleuven.be>
2020-03-18 16:02:51 +0100

    linux files

---
+++ do_script.m
@@ -0,0 +1,7 @@
+clear
+
+addpath_recurse('/home/r0625918/generative-surf-covers/generative')
+addpath_recurse('/home/r0625918/generative-surf-covers/matlab')
+addpath_recurse('/home/r0625918/generative-surf-covers/gptoolbox-master')
+
+gen_humans_dataset
