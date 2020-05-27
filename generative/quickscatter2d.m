c3f5ba22d5c297c8c6c8e3d730c9fe432a22d977
Rob Salaets <rob.salaets@telenet.be>
2020-05-27 17:18:33 +0200

    quickscatter edit

--- generative/quickscatter2d.m
+++ generative/quickscatter2d.m
@@ -1,7 +1,11 @@
 function quickscatter2d(V, m, c, varargin)
+    size = 5;
+    if ~isempty(varargin)
+        size=varargin{1};
+    end
     if m == 1
-        scatter(mod(V(:,1),1), mod(V(:,2),1), 5, c)
+        scatter(mod(V(:,1),1), mod(V(:,2),1), size, c)
     else
-        scatter(V(:,1),V(:,2), 5,c)
+        scatter(V(:,1),V(:,2), size,c)
     end
 end
